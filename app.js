3b1faf00cb680931160801869499785d289f834f
Andrew Rota <rota.andrew@gmail.com>
2015-05-28 14:26:54 -0400

    servo code pulled from rwaldron's example

--- app.js
+++ app.js
@@ -9,34 +9,59 @@ var board = new five.Board({
 
 board.on('ready', function() {
   var led = new five.Led('D7');
-  var bindKeys = function(ch, key) {
-    if (key && key.alt) {
-      if (key.name === 'l') {
-        console.log('lights');
+  var rudder = new five.Servo('A1');
+
+  rudder.center();
+
+  // see https://github.com/rwaldron/johnny_five_intro/blob/master/solutions/05_keyboard_led_and_servo.js
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function(ch, key) {
+
+    if (key.name === 'left') {
+      rudder.step(validateServoMove(10, rudder.position));
+    }
+
+    if (key.name === 'right') {
+      rudder.step(validateServoMove(-10, rudder.position));
+    }
+
+    if (key.name === 'space') {
+      console.log('...Centering Servo');
+      rudder.center();
+    }
+
+    if (key.name === 'up') {
+      // go forward
+    }
+
+    if (key.name === 'down') {
+      // go back
+    }
+
+    this.repl.inject({
+      l: function() {
         led.toggle();
-      } else if (key.name === 'w') {
-        console.log('forward');
-      } else if (key.name === 's') {
-        console.log('back');
-      } else if (key.name === 'a') {
-        console.log('left');
-      } else if (key.name === 'd') {
-        console.log('right');
       }
-    }
-    if (key && key.ctrl && key.name === 'c') {
-      process.stdin.pause();
-      process.exit();
-    }
-  };
-  this.repl.inject({
-    l: function() {
-      led.toggle();
-    }
+    });
+
   });
 
-  var rudder = new five.Servo('A1');
-  process.stdin.on('keypress', bindKeys);
+  function validateServoMove(adjustment, position) {
 
+    var newPosition = (position + adjustment);
+
+    if (newPosition < 1 || newPosition > 179) {
+      console.log('...Servo cannot be moved further in that direction');
+      adjustment = 0;
+    } else {
+      console.log('...Adjusting Servo to angle [' + (position += adjustment) + ']');
+    }
+
+    return adjustment;
+  }
 
 });
