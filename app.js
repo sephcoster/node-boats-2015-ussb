ed7a98b4cb396d00bfdc47f9934a25a241558d82
Andrew Rota <andrewrota@andrew-rota-pro.local>
2015-05-28 11:42:08 -0400

    Scaffold for keyboard stuff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var five = require("johnny-five");
-var Spark = require("spark-io");
+var five = require('johnny-five');
+var Spark = require('spark-io');
 var board = new five.Board({
   io: new Spark({
     token: '7e7b3a08075e683ee3e6eacd8d7c003b83b80b98',
@@ -7,8 +7,21 @@ var board = new five.Board({
   })
 });
 
-board.on("ready", function() {
-  var led = new five.Led("D7");
-  led.blink(500);
+board.on('ready', function() {
+  var led = new five.Led('D7');
+  process.stdin.on('keypress', function(ch, key) {
+    if (key && key.name === 'l') {
+      console.log('lights');
+      led.toggle();
+    } else if (key && key.name === 'w') {
+      console.log('forward');
+    } else if (key && key.name === 's') {
+      console.log('back');
+    } else if (key && key.name === 'a') {
+      console.log('left');
+    } else if (key && key.name === 'd') {
+      console.log('right');
+    }
+  });
 });
 
